dcf712e783b82669d9655b9f8016a889c8211c8f
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-05-25 11:48:21 +0600

    Toolbar created and links to register and Login containers added, Menus divide into 2: for unauthorised user and authorised one Facebook login component created and added to Login/Register containers

---
+++ frontend/src/components/UI/Toolbar/Toolbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavItem,
+    NavLink
+} from "reactstrap";
+import {NavLink as RouterNavLink} from "react-router-dom";
+import UserMenu from "./Menus/UserMenu";
+import AnonimousMenu from "./Menus/AnonimousMenu";
+
+const Toolbar = ({user, logout}) => {
+    return (
+        <Navbar color="light" light expand="md">
+            <NavbarBrand tag={RouterNavLink} to="/">Photo Gallery</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+                {/*{user && user.role === "admin" ?*/}
+                {/*<NavItem>*/}
+                {/*    <NavLink tag={RouterNavLink} to="/admin" exact>Admin dashboard</NavLink>*/}
+                {/*</NavItem> :*/}
+                <NavItem>
+                    <NavLink tag={RouterNavLink} to="/" exact>Photos</NavLink>
+                </NavItem>
+                {/*}*/}
+                {user ? <UserMenu user={user} logout={logout}/> : <AnonimousMenu/>}
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default Toolbar;
